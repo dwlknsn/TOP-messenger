05ee7bb25a982607289a49f465081a59a865dcb4
dwlknsn <dwlknsn@users.noreply.github.com>
2024-11-25 17:43:49 +0700

    Add hangouts page and messages channel; hook up consumer so we can broadcast messages

---
+++ app/javascript/channels/message_channel.js
@@ -0,0 +1,52 @@
+import consumer from "channels/consumer"
+
+const messageChannel = consumer.subscriptions.create("MessageChannel", {
+  connected() {
+    console.log('connected');
+    // Called when the subscription is ready for use on the server
+  },
+
+  disconnected() {
+    console.log('disconnected');
+    // Called when the subscription has been terminated by the server
+  },
+
+  received(data) {
+    // Called when there's incoming data on the websocket for this channel
+    console.log(data);
+
+    const messageDisplay = document.querySelector('#message-display')
+    messageDisplay.insertAdjacentHTML('beforeend', this.template(data))
+
+  },
+
+  template(data) {
+    return `<article class="message">
+              <div class="message-header">
+                <p>${data.user.email}</p>
+              </div>
+              <div class="message-body">
+                <p>${data.message.body}</p>
+              </div>
+            </article>`
+  }
+});
+
+document.addEventListener("turbo:load", () => {
+  let form = document.querySelector('#message-form')
+  console.log("Form loaded")
+  if(form) {
+    form.addEventListener('submit', (e) => {
+      e.preventDefault()
+
+      let messageInput = document.querySelector('#message-input').value
+
+      if(messageInput == '') return;
+      const message = {
+        body: messageInput
+      }
+
+      messageChannel.send({message: message})
+    })
+  }
+})
